91692d93471e6c1f3c37b67f326df490853ad285
Mateusz <tojagmerek@gmail.com>
2026-02-17 12:18:27 +0100

    new weapon list, fixing js

--- script.js
+++ script.js
@@ -28,8 +28,27 @@ const ui = {
 
 const keys = ["lvl","lp", "nlp","str", "dex", "mana", "hp", "h1", "h2", "bow", "cbow"];
 
+const limits = {
+    str: 1,
+    dex: 1,
+    mana: 1,
+    h1: 1,
+    h2: 1,
+    bow: 1,
+    cbow: 1
+}
+
+const state = {
+    weaponDmg: 5,
+    monsterDef: 0
+}
+
 function setStats(key) {
     ui[key].textContent = stats[key];
+    if(stats[key]>=120) limits[key] = 5;
+    else if(stats[key]>=90) limits[key] = 4;
+    else if(stats[key]>=60) limits[key] = 3;
+    else if(stats[key]>=30) limits[key] = 2;
 }
 
 function renderAll() {
@@ -43,9 +62,9 @@ document.querySelectorAll(".AddButton").forEach((btn) => {
 
         const beforeLvl = stats.lvl;
 
-        if(stats.lp>=delta && key != "lvl") {
+        if(stats.lp>=delta*limits[key] && key != "lvl") {
             stats[key] += delta;
-            stats.lp -= delta;
+            stats.lp -= delta * limits[key];
         }
 
         if(key === "lvl") {
@@ -57,7 +76,46 @@ document.querySelectorAll(".AddButton").forEach((btn) => {
         }
 
         renderAll();
-    })
-})
+    });
+});
+
+const weaponMenu = document.getElementById("weaponMenu");
+const weaponList = document.getElementById("weaponList");
+const backButton = document.getElementById("backButton");
+const weaponItems = document.getElementById("weaponItems");
+
+document.querySelectorAll(".weapon").forEach((btn) => {
+    btn.addEventListener("click", () => {
+        weaponList.classList.remove("hidden");
+        weaponMenu.classList.add("hidden");
+    });
+    btn.addEventListener("click", async () => {
+        const type = btn.dataset.attribute;
+
+        const res = await fetch("./assets/weapons.json");
+        const data = await res.json();
+        const filtered = data.filter(item => item.hands === type);
+
+        weaponItems.innerHTML = filtered.map(item => `<button class="AddButton chooseWeapon" data-dmg="${item.damage}">${item.name} - DMG: ${item.damage}</button>`).join("");
+
+        weaponItems.querySelectorAll(".chooseWeapon").forEach(btn => {
+        btn.addEventListener("click", () => {
+            if(stats.str>=btn.dataset.)
+            state.weaponDmg = Number(btn.dataset.dmg);
+    });
+    });
+    });
+});
+
+backButton.addEventListener("click", () => {
+    weaponList.classList.add("hidden");
+    weaponMenu.classList.remove("hidden");
+});
+
+
+
+function finalDmg() {
+    
+}
 
 renderAll();
